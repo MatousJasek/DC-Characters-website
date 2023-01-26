1d9dc5fff05c4669f71434421267a54d5fb6b5f7
MatousxJ <matous.jasek@icloud.com>
2023-01-26 13:43:24 +0100

    first commit

---
+++ buttonfunction.js
@@ -0,0 +1,24 @@
+function onClick(
+
+) {
+    const thing = document.getElementById ("demo")
+    if(thing.innerHTML != "rostik to je velky kostik") {
+        thing.innerHTML = "rostik to je velky kostik";
+    }
+    else {
+        thing.innerHTML = "who is Batman?";
+    }
+    console.log("Ahoj")
+}
+function onClick02 (
+
+) {
+    alert ("you discovered the magic!");
+}
+
+//function onClick02(
+
+//){
+    //const thing = document.getElementById ("demo02")
+    //if(thing.innerHTML == )
+
